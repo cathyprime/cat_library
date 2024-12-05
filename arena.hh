de5ee6248d00af92e00f6fb72f097d83a532cb23
cathyprime <magdajaroszynska47@gmail.com>
2024-12-05 11:07:18 +0100

    Arena fixes

--- arena.hh
+++ arena.hh
@@ -1,7 +1,7 @@
 #pragma once
 
 /*
- * arena.hh - v1.0.0 - Opinionated Queer License v1.2 - https://github.com/cathyprime/arena.hh
+ * arena.hh - v2.0.0 - Opinionated Queer License v1.2 - https://github.com/cathyprime/arena.hh
  *
  * Author: Magdalena Jaroszyńska
  * Last Edited: 10-11-2024
@@ -11,108 +11,76 @@ namespace cat {
 
 // TODO: allow for custom allocator
 
-#ifndef THROWING
-#define NOEXCEPT noexcept
-#define NULL_THROW(alloc) alloc
-#else
-#include <new>
-#define NOEXCEPT
-#define NULL_THROW(alloc) alloc; if (alloc == nullptr) throw new std::bad_alloc()
-#endif // endif THROWING
-
-#ifndef MEM_ARENA_DEFAULT
-#define MEM_ARENA_DEFAULT 64
+#ifndef CAT_MEM_ARENA_DEFAULT
+#define CAT_MEM_ARENA_DEFAULT 256
 #endif
 
-#ifndef size_t
 #include <stddef.h>
-#endif
 
-template<size_t arena_size = MEM_ARENA_DEFAULT>
-class Mem_Arena_Stack {
-    char arr[arena_size];
-    char *m_current = arr;
-    size_t m_remaining = arena_size;
-
-    void *alloc(size_t requested)
-    {
-        if (requested > m_remaining) return nullptr;
-        m_remaining -= requested;
-        char *ptr = m_current;
-        m_current += requested;
-        return ptr;
-    }
-    template<size_t sz>
-    friend void *operator new(size_t, Mem_Arena_Stack<sz>*) NOEXCEPT;
-
-  public:
-    Mem_Arena_Stack() = default;
-
-    size_t size()
-    {
-        return arena_size;
-    }
-
-    size_t remaining()
-    {
-        return m_remaining;
-    }
-};
+// TODO: Implement cat::Mem_Arena_Stack
 
 class Mem_Arena {
-    size_t m_remaining = MEM_ARENA_DEFAULT;
+    size_t m_capacity = CAT_MEM_ARENA_DEFAULT;
     char *m_data;
     char *m_current;
 
-    void *alloc(size_t size)
+    using Dtor_Ptr = void(*)(const void*);
+
+    struct metadata_t {
+        size_t size;
+        Dtor_Ptr dtor;
+    };
+
+  public:
+    template<typename T> void *alloc()
     {
-        if (size > m_remaining) return nullptr;
-        m_remaining -= size;
-        char *ptr = m_current;
+        constexpr size_t size = sizeof(T);
+        constexpr size_t metadata_size = sizeof(metadata_t);
+
+        if (size > m_capacity)
+            return nullptr;
+
+        metadata_t *meta = (metadata_t*)m_current;
+        *meta = {
+            .size= size,
+            .dtor = [](const void *ptr) {
+                static_cast<const T*>(ptr)->~T();
+            }
+        };
+        m_current += metadata_size;
+
+        void *ptr = m_current;
         m_current += size;
         return ptr;
     }
-    friend void *operator new(size_t, Mem_Arena*) NOEXCEPT;
 
-  public:
-    Mem_Arena() :
-      m_data((char*)malloc(m_remaining)),
-      m_current(m_data)
+    Mem_Arena()
+      : m_data((char*)malloc(m_capacity))
+      , m_current(m_data)
     {};
 
-    Mem_Arena(size_t size) :
-      m_remaining(size),
-      m_data((char*)malloc(m_remaining)),
-      m_current(m_data)
+    Mem_Arena(size_t size)
+      : m_capacity(size)
+      , m_data((char*)malloc(m_capacity))
+      , m_current(m_data)
     {};
 
     size_t size()
     {
-        return (m_current - m_data) + m_remaining;
-    }
-
-    size_t remaining()
-    {
-        return m_remaining;
+        return (size_t)m_capacity;
     }
 
     ~Mem_Arena()
     {
+        constexpr size_t metadata_size = sizeof(metadata_t);
+        char *pos = m_data;
+        while (pos < m_current) {
+            metadata_t *meta = (metadata_t*)pos;
+            meta->dtor(pos + metadata_size);
+            pos += meta->size + metadata_size;
+        }
         free(m_data);
     }
 };
-
-template<size_t arena_size>
-void *operator new(size_t size, Mem_Arena_Stack<arena_size> *arena) NOEXCEPT
-{
-    void *ptr = NULL_THROW(arena->alloc(size));
-    return ptr;
-}
-
-inline void *operator new(size_t size, Mem_Arena *arena) NOEXCEPT
-{
-    void *ptr = NULL_THROW(arena->alloc(size));
-    return ptr;
-}
 
 }
